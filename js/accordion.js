fdfd1662e42cf6be09bca4079bdc6387cfdafcd5
angelgarcia <angelrgarciajimenez@gmail.com>
2025-09-27 19:53:17 -0500

    add img - css - index mejorado estructurado

---
+++ js/accordion.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const headers = document.querySelectorAll(".accordion-header");
+  headers.forEach(header => {
+    header.addEventListener("click", () => {
+      const body = header.nextElementSibling;
+      body.style.display = body.style.display === "block" ? "none" : "block";
+    });
+  });
+});
